32dde41d118632e0ec1af3d2ff9e57228560b789
ian <99942apophis@tutamail.com>
2021-01-05 21:46:49 -0500

    Redid the page on typeclasses
    I think I'm starting to understand it better now!

--- notes.hs
+++ notes.hs
@@ -111,6 +111,8 @@ module Geometry.Cuboid
 
 -- ================== DATA TYPES =====================
 
+-- Types and Typeclasses DAY TWO
+
 -- The circle value constructor has 3 parameters. Value constructors are
 -- actually functions that ultimately return a value of a data type.
 data Shape = Circle Float Float Float | Rectangle Float Float Float Float
@@ -121,6 +123,7 @@ surface (Circle _ _ r) = pi * r ^ 2
 surface (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
 
 {-
+
 Exporting a type is pretty simple:
 
 module Shapes
@@ -137,6 +140,11 @@ be done by including them without the `(..)`
 
 -}
 
+-- ============= RECORD SYNTAX ====================
+
+-- The info we want to store is:
+-- first name, last name, age, height, phone number, and favourite ice-cream
+-- flavour.
 data Person = Person String String Int Float String String deriving (Show)
 
 flavor :: Person -> String
@@ -151,6 +159,45 @@ data PersonRecord = PersonRecord { firstName :: String
 -- This is just syntactic sugar for creating functions manually as we did in
 -- the above case
 
+-- ======================= TYPE PARAMETERS ===============================
+
+-- A value constructor can take some values parameters and then produce a new
+-- value. In a similar manner, _type constructors_ can take types as parameters
+-- to produce new types.
+--
+-- e.g.
+-- data Maybe a = Nothing | Just a
+--
+-- Because there is a type parameter involved, we call Maybe a type
+-- constructor. No value can have a type of just Maybe, because that's not a
+-- concrete type (*), it's a type constructor.
+--
+--
+-- Typeclass constraints are mentioned in the Learn you a Haskell, but have
+-- been removed from the language.
+
+-- ======================== DERIVED INSTANCES =================================
+--
+-- Haskell can automatically make types instances of Eq, Ord, Enum, Bounded,
+-- Show, Read using the 'deriving' keyword.
+--
+
+data Day = Montag | Dienstag | Mittwoch | Donnerstand | Freitag | Samstag | Sonntag
+           deriving(Eq, Ord, Show, Read, Bounded, Enum)
+
+-- When you do something like:
+-- read "Samstag" :: Day, what the heck is going on?
+--
+-- The type of read is:
+--
+-- read :: Read a => String -> a
+--
+-- The kind of Read is:
+--
+-- Read :: * -> Constraint
+--
+-- :: seems to be some sort of type declaration thingey
+
 
 {-
     Type aliases are also a thing!
@@ -158,6 +205,43 @@ data PersonRecord = PersonRecord { firstName :: String
     type String = [Char]
 
 -}
+
+-- You can partially apply type parameters and get new type constructors.
+-- e.g.
+-- type IntMap = Map.Map Int
+-- This one cannot be done though, because Map.Map's Data constructor is not
+-- in scope
+
+
+-- Lets look at more cool data types:
+-- data Either a b = Left a | Right b deriving(Eq, Ord, Read, Show)
+
+-- Lets do a map example!
+data LockerState = Taken | Free deriving (Show, Eq)
+
+type Code = String
+
+type LockerMap = Map.Map Int (LockerState, Code)
+
+lockerLookup :: Int -> LockerMap -> Either String Code
+lockerLookup lockerNumber map =
+    case Map.lookup lockerNumber map of
+      Nothing -> Left $ "Locker number " ++ show lockerNumber ++ " doesn't exist!"
+      Just (state, code) -> if state /= Taken
+                               then Right code
+                               else Left $ "Locker " ++ show lockerNumber ++ " is already taken!"
+
+lockers :: LockerMap
+lockers = Map.fromList
+    [(100,(Taken,"ZD39I"))
+    ,(101,(Free,"JAH3I"))
+    ,(103,(Free,"IQSA9"))
+    ,(105,(Free,"QOTSA"))
+    ,(109,(Taken,"893JJ"))
+    ,(110,(Taken,"99292"))
+    ]
+
+-- ======================== RECURSIVE DATA STRUCTURES ==========================
 
 data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
 
@@ -194,28 +278,102 @@ instance Show TrafficLight where
     show Yellow = "Yellow light"
     show Green = "Green light"
 
+--
+-- You can't do something like:
+--
+-- instance Eq Maybe where
+--  ...
+-- But you can do something like
+--
+-- instance (Eq m) => Eq (Maybe m) where
+--     Just x == Just y = x == y
+--     Nothing == Nothing = True
+--     _ == _ = False
+--
+-- This instance declaration says that:
+-- We want all types of the form `Maybe m` to be part of the `Eq` typeclass,
+-- but only those types where the `m` is also a part of `Eq`.
+--
+
+class YesNo a where
+    yesno :: a -> Bool
+
+instance YesNo Int where
+    yesno 0 = False
+    yesno _ = True
+
+instance YesNo [a] where
+    yesno [] = False
+    yesno _ = True
+
 -- The Functor Typeclass
 -- Basically for things that can be mapped over.
 
 {-
+
 class Functor f where
     fmap :: (a -> b) -> f a -> f b
 
-fmap takes a function from one type to another, and a functor
-applied with one type, and returns a functor applied with another
-type.
+In the definitions of typeclasses we've seen so far, the type variable was a
+concrete type. e.g.
+(==) :: (Eq a) => a -> a -> Bool
+
+But now, the `f` is not a concrete type, but a type constructor that takes one
+parameter.
+
+fmap takes a function from one type to another, and a functor applied with one
+type, and returns a functor applied with another type.
 
 It's similar to the signature for map:
 map :: (a -> b) -> [a] -> [b]
+
+Indeed, map is fmap that works only on lists!!
 -}
+
+-- Lets try to write an instance of Functor for Tree.
+instance Functor Tree where
+    fmap f EmptyTree = EmptyTree
+    fmap f (Node x l r) = Node (f x) (fmap f l) (fmap f r)
 
 {-
 
 The way Map is a functor probably looks something like this:
 
-instance Functor Map where
-    fmap f (Map m) = fromList $ map (\(k, v) -> (k, f v)) $ toList m
+instance Functor Map.Map where
+    fmap f Map.Empty = Map.Empty
+    fmap f (Map.Map m) = fromList $ map (\(k, v) -> (k, f v)) $ toList m
+
+First, the Map is converted to a list, then map a lambda transforming all the
+key value pairs into the new type, then construct a new map from the list.
+
 -}
 
--- Tomorrow!
--- Input and Output!!
+-- ===================================== KINDS =================================
+
+-- use :k to lookup the kind.
+-- * is a concrete type
+--
+-- Maybe is a type constructor, so its kind is:
+-- * -> *
+--
+-- That is, it takes a concrete type and returns a concrete type
+--
+-- Maybe Int would have kind `*`
+--
+
+-- Type foo time
+class Tofu t where
+    tofu :: j a -> t a j
+
+-- How the heck would we instance this?
+-- a is a *, so j is * -> *, therefore t must be `* -> (* -> *) -> *`
+
+data Frank a b = Frank {frankField :: b a} deriving (Show)
+
+-- Frank is a type constructor that takes a concrete type, and a type constructor
+-- `* -> (* -> *) -> *` exactly.
+
+instance Tofu Frank where
+    tofu = Frank
+
+-- This is a bit brain melty
